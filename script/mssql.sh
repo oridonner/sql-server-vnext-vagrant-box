f9c86351f37781481b75fa2f878e73550d84a066
Ori Donner <oridonner@gmail.com>
2017-04-06 12:07:32 +0300

    Edit README file

--- script/mssql.sh
+++ script/mssql.sh
@@ -2,11 +2,6 @@
 
 set -eux
 
-# Sample custom configuration script - add your own commands here
-# to add some additional commands for your environment
-#
-# For example:
-# yum install -y curl wget git tmux firefox xvfb
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/mssql-server.list | sudo tee /etc/apt/sources.list.d/mssql-server.list
 
